df6614a93a4bf4d14841cb8bcd35243f66556720
Joshua Nice <joshliamnice@gmail.com>
2019-04-27 12:01:56 +0100

    fixes

--- js/jqm_defaults.js
+++ js/jqm_defaults.js
@@ -1,15 +1,29 @@
 // JavaScript Document
-jQuery(document).bind("mobileinit", function(){
- 
-console.log('mobiel init');
-jQuery.mobile.changePage.defaults.changeHash = true;
-        jQuery.mobile.hashListeningEnabled = true;
-        jQuery.mobile.pushStateEnabled = false;
-		jQuery.support.cors = true;
-   		jQuery.mobile.allowCrossDomainPages = true;
-		jQuery.mobile.changePage.defaults.allowSamePageTransition = true;
-		jQuery.mobile.phonegapNavigationEnabled = true;
-		jQuery.mobile.defaultPageTransition='none';
-		FastClick.attach(document.body);
-		
+jQuery(document).bind("mobileinit", function () {
+
+	console.log('mobile init');
+	//look these up as they will have an effect on performence
+	jQuery.mobile.changePage.defaults.changeHash = true;
+	jQuery.mobile.hashListeningEnabled = true;
+	jQuery.mobile.pushStateEnabled = false;
+	jQuery.support.cors = true;
+	jQuery.mobile.allowCrossDomainPages = true;
+	jQuery.mobile.changePage.defaults.allowSamePageTransition = false;
+
+	//IMPORTANT
+	jQuery.mobile.phonegapNavigationEnabled = true;
+	//IMPORTANT
+	jQuery.mobile.defaultPageTransition = 'flow';
+	/*
+	fade
+	pop
+	flip
+	turn
+	flow
+	slidefade
+	slide
+	slideup
+	slidedown
+	*/
+
 });
