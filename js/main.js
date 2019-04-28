aac316c1b0cde1ed28f06d8cd8cfa530b2f575f9
Josh <joshliamnice@gmail.com>
2019-04-28 13:41:33 +0100

    fixes

--- js/main.js
+++ js/main.js
@@ -17,4 +17,47 @@ $(document).ready(function () {
 
 
 function innit() {
+
+	$("#submit-item").click(function() {
+		console.log("add item button clicked");
+
+		let item = {
+			item_type : $("#item-type").val(),
+			item_name : $("#input-name-val").val(),
+			item_amount : $("#input-amount-val").val(),
+			item_cat : $("#item-cat").val()
+		}
+
+		let unstring_items = JSON.parse(localStorage.getItem("items"));
+
+		if (unstring_items === null)
+		{
+			let arr_item = [];
+			arr_item.push(item);
+			localStorage.setItem("items",JSON.stringify(arr_item));
+		}
+		else 
+		{
+			unstring_items.push(item);
+			localStorage.setItem("items",JSON.stringify(unstring_items));
+		}
+
+	});
+
+	$("#trans").click(function() {
+		console.log("transaction button clicked");
+
+		let unstring_items = JSON.parse(localStorage.getItem("items"));
+		console.log(unstring_items);
+
+		for (var index = 0; index < unstring_items.length; index++)
+		{
+			//console.log(unstring_items[index].item_name);
+			document.getElementById("item-name-dis").innerHTML = unstring_items[index].item_name;
+		}
+
+		//document.getElementById("item-name-dis").innerHTML = item_name;
+
+	});
+
 }
