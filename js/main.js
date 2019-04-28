4fbb06a64ebda2f5b76ffc178926b1f34d1e0a0f
Josh <joshliamnice@gmail.com>
2019-04-28 15:34:06 +0100

    Fixes

--- js/main.js
+++ js/main.js
@@ -41,10 +41,15 @@ function innit() {
 			localStorage.setItem("items",JSON.stringify(unstring_items));
 		}
 
+		swal("Good job!", "Item Submitted", "success");
+
+
 	});
 
 	$("#trans").click(function() {
 		console.log("transaction button clicked");
+
+		$("#item-data").empty();
 
 		let unstring_items = JSON.parse(localStorage.getItem("items"));
 		console.log(unstring_items);
@@ -55,8 +60,29 @@ function innit() {
 			let amount = unstring_items[index].item_amount;
 			let cat = unstring_items[index].item_cat;
 			let type = unstring_items[index].item_type;
-			$( "#item-data" ).append(`<div class="row"><div class="col-3">${name}</div><div class="col-3">${amount}</div><div class="col-3">${cat}</div><div class="col-3">${type}</div></div>`);
+			$("#item-data").append(`<div class="row"><div class="col-3">${name}</div><div class="col-3">${amount}</div><div class="col-3">${cat}</div><div class="col-3">${type}</div></div>`);
 		}
+	});
+
+	$("#clear-stor").click(function() {
+		swal({
+			title: "Are you sure you want to clear the storage?",
+			text: "",
+			icon: "warning",
+			buttons: true,
+			dangerMode: true,
+		  })
+		  .then((willDelete) => {
+			if (willDelete) {
+			  swal("Items deleted!", {
+				icon: "success",
+			  });
+			  localStorage.clear();
+			} else {
+			  swal("Items are safe!");
+			}
+		  });
+		
 	});
 
 }
