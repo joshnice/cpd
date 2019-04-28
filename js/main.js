57eda555dc411a4848245766b54da8e63d19cc04
Josh <joshliamnice@gmail.com>
2019-04-28 16:36:15 +0100

    fixes

--- js/main.js
+++ js/main.js
@@ -43,6 +43,8 @@ function innit() {
 
 		swal("Good job!", "Item Submitted", "success");
 
+		document.getElementById('myInput').value = ' ';
+
 
 	});
 
@@ -60,7 +62,7 @@ function innit() {
 			let amount = unstring_items[index].item_amount;
 			let cat = unstring_items[index].item_cat;
 			let type = unstring_items[index].item_type;
-			$("#item-data").append(`<div class="row"><div class="col-3">${name}</div><div class="col-3">${amount}</div><div class="col-3">${cat}</div><div class="col-3">${type}</div></div>`);
+			$("#item-data").append(`<div class="row" style="margin-top:15px;"><div class="col-3">${name}</div><div class="col-3">${amount}</div><div class="col-3">${cat}</div><div class="col-3">${type}</div></div>`);
 		}
 	});
 
@@ -82,6 +84,71 @@ function innit() {
 			  swal("Items are safe!");
 			}
 		  });
+		
+	});
+
+
+	$("#bud").click(function() {
+		console.log("budget navi clicked");
+		console.log(localStorage.getItem("budget-target"));
+		console.log(localStorage.getItem("budget-starting"));
+		
+		document.getElementById("target-value").innerHTML = localStorage.getItem("budget-target");
+		document.getElementById("starting-value").innerHTML = localStorage.getItem("budget-starting");
+
+		let unstring_items = JSON.parse(localStorage.getItem("items"));
+		console.log(unstring_items);
+
+		let total = 0;
+		let net = 0;
+
+		if (unstring_items === null)
+		{
+			console.log("no items in items")
+		}
+		else
+		{
+			for (var index = 0; index < unstring_items.length; index++)
+			{
+				let amount = unstring_items[index].item_amount;
+				let type = unstring_items[index].item_type;
+
+				console.log("amount", amount);
+
+				if (type == "income")
+				{
+					total += amount;
+				}
+				else 
+				{
+					total -= amount;
+				}
+
+				console.log("total", total);
+			}
+
+			console.log("total", total);
+
+			document.getElementById("current-value").innerHTML = total;
+
+			let starting = localStorage.getItem("budget-starting");
+
+			starting = Number(starting);
+
+			net = starting + total;
+			console.log(net);
+
+			document.getElementById("net-value").innerHTML = net;
+		}
+
+
+	});
+
+	$("#submit-budget").click(function() {
+		console.log("submit budget clicked");
+
+		localStorage.setItem("budget-target", $("#input-target-val").val());
+		localStorage.setItem("budget-starting", $("#input-starting-val").val());
 		
 	});
 
